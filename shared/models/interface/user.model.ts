0fd79eeda1ee9c77abb8550a2bb5e466157c64df
pam5596 <ryosuke3191@gmail.com>
2026-07-10 12:32:01 +0900

    #10 ユーザーモデルを実装

---
+++ shared/models/interface/user.model.ts
@@ -0,0 +1,37 @@
+import z from "zod";
+import { BaseModel } from "./_base";
+import type { User } from "./user.interface";
+
+export class UserModel extends BaseModel<User> implements User {
+  // [!] ロジック次第でreadonlyは外してOK
+  readonly id?: number;
+  readonly channel_id!: string;
+  readonly name!: string;
+  readonly avatar!: string;
+  readonly created_at?: Date;
+  
+  constructor(user: User) {
+    super(user, UserModel.schema())
+    Object.assign(this, user)
+  }
+
+  private static schema() {
+    return z.object({
+      id: z.int().optional(),
+      channel_id: z.string().length(24),
+      name: z.string().min(1),
+      avatar: z.url({ protocol: /^https?$/ }),
+      created_at: z.date().optional()
+    })
+  }
+
+  override toObject(): User {
+    return {
+      id: this.id,
+      channel_id: this.channel_id,
+      name: this.name,
+      avatar: this.avatar,
+      created_at: this.created_at
+    }
+  }
+}
---
+++ test/unit/model/interface/uset.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "vitest";
+import { UserModel } from "../../../../shared/models/interface/user.model";
+
+describe("UserModelの単体テスト", () => {
+  const values = {
+    id: 1,
+    channel_id: 'abcdefgabcdefgabcdefgabc',
+    name: 'name',
+    avatar: 'https://example.com',
+    created_at: new Date()
+  }
+
+  it("モデルが作成できる", () => {
+    expect(() => new UserModel(values)).not.toThrow()
+
+    const { id, created_at, ...require_params} = values
+    expect(() => new UserModel(require_params)).not.toThrow()
+  })
+
+  it("channel_idでエラーになる", () => {
+    expect(() => new UserModel({ ...values, channel_id: "abc"})).toThrow()
+  })
+
+  it("nameでエラーになる", () => {
+    expect(() => new UserModel({ ...values, name: ""})).toThrow()
+  })
+
+  it("avatarでエラーになる", () => {
+    expect(() => new UserModel({ ...values, avatar: "http"})).toThrow()
+  })
+
+  it("toObjectメソッドがobjectを返す", () => {
+    expect(new UserModel(values).toObject()).toEqual(values)
+  })
+})
