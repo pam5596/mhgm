296326cec70cf9fa788d1d98ca945b8fd652fd79
pam5596 <ryosuke3191@gmail.com>
2026-07-17 01:22:18 +0900

    #15 KeywordsPOSTRequestDTOを実装

---
+++ shared/dtos/interfaces/keywords.post.req.d.ts
@@ -0,0 +1,10 @@
+import type { Setting } from "../../shared/models/interfaces/setting.interface";
+import type { BaseDTOInterface } from "./_base";
+import type { Keyword } from "../../shared/models/interfaces/keyword.interface";
+
+export interface KeywordsPOSTRequest implements BaseDTOInterface {
+  body: {
+    keyword: Keyword["keyword"],
+    action: Keyword["action"]
+  }
+}
