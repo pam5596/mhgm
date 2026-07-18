a4ad7f9d32df4e0370269efd4584083a084e91b3
pam5596 <ryosuke3191@gmail.com>
2026-07-19 02:44:25 +0900

    #14 BroadcastsPUTRequestを実装

---
+++ server/services/put_broadcast.service.ts
@@ -0,0 +1,27 @@
+import { BaseService } from "./_base";
+import { BroadcastsPUTRequestDTO } from "../../shared/dtos/broadcasts.put.req.dto";
+import { BroadcastsPUTResponseDTO } from "../../shared/dtos/broadcasts.put.res.dto";
+import { BroadcastRepository } from "../repositories/broadcast.repository";
+import { BroadcastModel } from "../../shared/models/broadcast.model";
+
+export class PUTBroadCastService implements BaseService<
+  BroadcastsPUTRequestDTO,
+  BroadcastsPUTResponseDTO
+> {
+  constructor(
+    private broadcastRepository: BroadcastRepository
+  ) {}
+
+  async execute(request: BroadcastsPUTRequestDTO) {
+    const { user_id } = request.values.sessions.user
+    const broadcast = await this.broadcastRepository.upsert(
+      new BroadcastModel({
+        ...request.values.body,
+        user_id,
+        end_at: null
+      })
+    )
+
+    return new BroadcastsPUTResponseDTO({ body: { id: broadcast.id } })
+  }
+}
