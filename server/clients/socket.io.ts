bcbb05df75ae6e0b6b6d5a1a9810180b0871874f
pam5596 <ryosuke3191@gmail.com>
2026-07-19 15:05:58 +0900

    #14 SocketIOにnamespace取得用ゲッターを追加

--- server/clients/socket.io.ts
+++ server/clients/socket.io.ts
@@ -5,4 +5,12 @@ export class SocketIOClient extends Server {
   constructor(server: HTTPServer, opts?: Partial<ServerOptions>) {
     super(server, opts)
   }
+
+  get liveChat() {
+    return this.of("/live-chat")
+  }
+
+  get member() {
+    return this.of("/member")
+  }
 }
