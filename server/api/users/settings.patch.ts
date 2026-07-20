7a06fbd9bf7391833df320483d656d07e1d8b803
pam5596 <ryosuke3191@gmail.com>
2026-07-20 17:51:21 +0900

    #18 /users/settingsのPATCHイベントハンドラーを実装

---
+++ server/api/users/settings.patch.ts
@@ -0,0 +1,17 @@
+import { UsersSettingsPATCHRequestDTO } from "~~/shared/dtos/users_settings.patch.req.dto"
+
+export default defineEventHandler(async (event) => {
+  const { user } = await getUserSession(event)
+  const body = await readBody(event)
+
+  await new PATCHUserSettingsService(
+    settingRepository
+  ).execute(
+    new UsersSettingsPATCHRequestDTO({
+      sessions: {
+        user: { user_id: user!.user_id }
+      },
+      body
+    })
+  )
+})
