78ecf4383ce0e46b4bebb249aa1296a5455c9eb7
pam5596 <ryosuke3191@gmail.com>
2026-08-08 14:57:46 +0900

    #34 Buttonコンポーネントを実装

---
+++ app/components/atm/Button.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-btn 
+    v-bind="$attrs"
+    class="font-bold p-2"
+    :class="styles"
+  >
+    <div class="flex gap-2 items-center">
+      <Icon v-if="props.icon" :name="props.icon" class="size-5" />
+      <span>
+        <slot />
+      </span>
+    </div>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+defineOptions({ inheritAttrs: false })
+const props = defineProps<{
+  color: 'primary' | 'success' | 'error'
+  variant: 'fill' | 'outlined'
+  icon?: string
+}>()
+
+const styles = computed(() => [
+  {
+    "gradient-primary": props.color === 'primary',
+    "gradient-success": props.color === 'success',
+    "gradient-error": props.color === 'error'
+  },
+  props.variant === 'outlined' && {
+    "text-primary": props.color === 'primary',
+    "text-success": props.color === 'success',
+    "text-error": props.color === 'error'
+  },
+  {
+    "text-white": props.variant === 'fill',
+    "border-gradient-mode": props.variant === 'outlined',
+    "bg-gradient-image": props.variant === 'fill'
+  }
+])
+
+
+</script>
+
+<style scoped>
+
+</style>
