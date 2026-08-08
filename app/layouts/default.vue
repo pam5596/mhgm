6a3a596986d8125be257ae2fb6c07a6dd7d0b2b3
pam5596 <ryosuke3191@gmail.com>
2026-08-08 14:58:52 +0900

    #34 defaultレイアウトにmax-widthを追加

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="w-screen h-screen px-8 py-4 bg-gradient-to-b from-[#EBE5D8] to-[#FAF8F5] flex flex-col justify-between">
-    <div class="flex flex-col gap-8 h-full">
-      <header class="w-full flex flex-row justify-between">
-        <MolLockup />
-        <div class="flex flex-row items-center gap-3">
-          <slot name="header" />
+  <div class="w-screen h-screen bg-gradient-to-b from-[#EBE5D8] to-[#FAF8F5]">
+    <div class="w-full h-full flex justify-center">
+      <div class="px-8 py-4 w-full max-w-screen-xl flex flex-col gap-8">
+        <div class="flex flex-col gap-8 h-full">
+          <header class="w-full flex flex-row justify-between">
+            <MolLockup />
+            <div class="flex flex-row items-center gap-3">
+              <slot name="header" />
+            </div>
+          </header>
+          <main class="w-full h-full flex flex-col justify-center">
+            <slot name="main" />
+          </main>
         </div>
-      </header>
-      <main class="w-full h-full flex flex-col justify-center">
-        <slot name="main" />
-      </main>
+        <footer class="w-full flex flex-col items-center gap-2 text-xs text-default">
+          <MolCopyright :links="links" />
+        </footer>
+      </div>
     </div>
-    <footer class="w-full flex flex-col items-center gap-2 text-xs text-default-black">
-      <MolCopyright :links="links" />
-    </footer>
   </div>
 </template>
 
 <script setup lang="ts">
 const links = [
-  { label: "ui.footer.links.youtube_terms", href: "https://www.youtube.com/t/terms" },
-  { label: "ui.footer.links.google_privacy", href: "https://policies.google.com/privacy" },
-  { label: "ui.footer.links.github", href: "https://github.com/pam5596/mhgm" },
+  { label: "layouts.default.links.youtube_terms", href: "https://www.youtube.com/t/terms" },
+  { label: "layouts.default.links.google_privacy", href: "https://policies.google.com/privacy" },
+  { label: "layouts.default.links.github", href: "https://github.com/pam5596/mhgm" },
 ];
 </script>
 
