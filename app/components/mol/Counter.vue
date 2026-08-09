7c6e05856d6486265e5048a72b010ad8c8947d18
pam5596 <ryosuke3191@gmail.com>
2026-08-09 18:08:10 +0900

    #39 Counterコンポーネントを実装

---
+++ app/components/mol/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex gap-1">
+    <AtmButton color="success" variant="outlined" icon="ic:baseline-plus" />
+    <input
+      type="text"
+      class="gradient-success border-gradient-mode rounded-md p-1 h-9 w-10 text-center text-default"
+    />
+    <AtmButton color="success" variant="outlined" icon="ic:baseline-minus" />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped>
+
+</style>
