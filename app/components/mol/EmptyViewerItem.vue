b7d3895fef797497b418c1616bb7d368abb53779
pam5596 <ryosuke3191@gmail.com>
2026-08-09 18:08:23 +0900

    #39 EmptyViewerItemコンポーネントを実装

---
+++ app/components/mol/EmptyViewerItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center px-4 py-1 border-2 border-dotted border-[#14B8A6] rounded-xl shadow-lg h-16">
+    <p class="text-default font-bold line-clamp-2 text-success">
+      <slot />
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
