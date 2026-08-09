2e3e4ee1e2c7a26f9b1c3d48f630a4d486e983b0
pam5596 <ryosuke3191@gmail.com>
2026-08-09 18:08:38 +0900

    #39 JoinerItemコンポーネントを実装

---
+++ app/components/mol/JoinerItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <AtmCard color="success" class="flex justify-between px-4 py-1 gap-2 h-16">
+    <div class="flex gap-2 items-center">
+      <AtmIconButton color="success" variant="outlined">
+        <v-avatar :image="props.avatar" />
+      </AtmIconButton>
+      <p class="text-default font-bold max-w-56 line-clamp-2">
+        {{ props.name }}
+      </p>
+    </div>
+    <div class="flex items-center gap-2">
+      <div class="flex flex-col items-center">
+        <span class="text-success font-bold text-xs">参加クエスト数</span>
+        <MolCounter v-model="model" />
+      </div>
+      <AtmButton color="success" variant="fill">
+        交代
+      </AtmButton>
+      <AtmButton color="error" variant="fill">
+        退出
+      </AtmButton>
+    </div>
+  </AtmCard>
+</template>
+
+<script setup lang="ts">
+const model = defineModel<number>()
+const props = defineProps<{
+  name: string
+  channel_id: string
+  avatar: string
+}>()
+
+</script>
+
+<style scoped>
+
+</style>
