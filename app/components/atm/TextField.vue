16ec1de14732d896d195c91aa967a8c2022a837f
pam5596 <ryosuke3191@gmail.com>
2026-08-09 18:08:01 +0900

    #39 TextFieldコンポーネントを実装

---
+++ app/components/atm/TextField.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-col">
+    <input
+      type="text"
+      class="gradient-primary border-gradient-mode rounded-md p-2"
+      :class="styles"
+      :placeholder="props.placeholder"
+      v-model="model"
+    />
+    <span v-if="props.error" class="text-error">
+      {{ props.error }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const model = defineModel<string>()
+
+const props = defineProps<{
+  placeholder?: string
+  error?: string
+}>()
+
+const styles = computed(() => ([
+  {
+    "gradient-error": props.error
+  }
+]))
+
+</script>
+
+<style scoped>
+
+</style>
