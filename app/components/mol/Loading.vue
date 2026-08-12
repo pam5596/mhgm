f8ea38ff508fd979a89e9d2085fac685bfb3332d
pam5596 <ryosuke3191@gmail.com>
2026-08-13 00:48:45 +0900

    #44 Loadingコンポーネントを実装

---
+++ app/components/mol/Loading.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-overlay
+    :model-value="props.isShow"
+    class="align-center justify-center z-[2999]"
+    persistent
+  >
+    <v-progress-circular
+      color="white"
+      size="64"
+      width="4"
+      indeterminate
+    ></v-progress-circular>
+  </v-overlay>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  isShow: boolean
+}>()
+</script>
+
+<style scoped>
+
+</style>
