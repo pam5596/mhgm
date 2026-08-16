5dae970e28432824f82f515b5321b4557ae8e747
pam5596 <ryosuke3191@gmail.com>
2026-08-16 15:36:53 +0900

    #51 Counterのロジックを実装

--- app/components/mol/Counter.vue
+++ app/components/mol/Counter.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="flex gap-1">
-    <AtmButton :color="props.color" variant="outlined" icon="ic:baseline-plus" />
+    <AtmButton :color="props.color" variant="outlined" icon="ic:baseline-minus" @click="decreace"/>
     <input
-      type="text"
-      class="rounded-md p-1 h-9 w-10 text-center"
-      :class="styles"
+    type="text"
+    class="rounded-md p-1 h-9 w-10 text-center"
+    :class="styles"
+    v-model="model"
     >
-    <AtmButton :color="props.color" variant="outlined" icon="ic:baseline-minus" />
+    <AtmButton :color="props.color" variant="outlined" icon="ic:baseline-plus" @click="increace"/>
   </div>
 </template>
 
 <script setup lang="ts">
+const model = defineModel<number>({ default: 0 })
+
 const props = withDefaults(
   defineProps<{
     color?: 'primary' | 'success' | 'error'
+    max?: number
+    min?: number
   }>(),
   {
-    color: "primary"
+    color: "primary",
+    max: Infinity,
+    min: -Infinity
   }
 )
 const styles = computed(() => [
@@ -26,6 +33,12 @@ const styles = computed(() => [
     "border-gradient-error": props.color === "error"
   }
 ])
+
+const increace = () => { if(model.value <= props.max) model.value++ }
+
+const decreace = () => { if(props.min <= model.value) model.value-- }
+
+
 
 </script>
 
