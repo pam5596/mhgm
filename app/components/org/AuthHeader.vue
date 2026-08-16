b7b70499c18b1dcccccf4a0f742fff465616121c
pam5596 <ryosuke3191@gmail.com>
2026-08-16 12:47:42 +0900

    #50 authheaderのログアウト処理を実装

--- app/components/org/AuthHeader.vue
+++ app/components/org/AuthHeader.vue
@@ -1,10 +1,16 @@
 <template>
-  <AtmButton color="primary" variant="outlined" icon="ic:baseline-logout">
+  <AtmButton color="primary" variant="outlined" icon="ic:baseline-logout" @click="onLogout">
     {{ $t("layouts.default.button.logout") }}
   </AtmButton>
 </template>
 
 <script setup lang="ts">
+const { clear } = useUserSession()
+
+const onLogout = async () => {
+  await clear()
+  navigateTo("/")
+}
 
 </script>
 
