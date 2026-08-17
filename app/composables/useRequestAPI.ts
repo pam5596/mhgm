080dde818fefe310bc344681a4bb9f688d9dcfde
pam5596 <ryosuke3191@gmail.com>
2026-08-17 09:26:27 +0900

    #51 requestAPIのエラーハンドリングを修正

--- app/composables/useRequestAPI.ts
+++ app/composables/useRequestAPI.ts
@@ -10,7 +10,7 @@ export default function () {
       loadingMessage?: string
       successMessage?: string
     }
-  ): Promise<{ data: ResT | null, error: Error | null }> {
+  ) {
     console.debug(request)
     if (opts?.showLoading) toggleLoading()
     if (opts?.loadingMessage) showAlert({
@@ -18,27 +18,43 @@ export default function () {
       title: opts.loadingMessage
     })
 
-    try {
-      const data = await $fetch<ResT>(request, opts)
-      console.debug(data)
-      if (opts?.successMessage) showAlert({
-        type: "success",
-        title: opts.successMessage
-      })
-
-      return { data: data ?? null, error: null }
-    } catch (e) {
-      const error = e as Error
-      console.error(error)
-      showAlert({
-        type: "error",
-        title: error?.message ?? t("errors.default"),
-        error: error && errorToString(error)
-      })
-      
-      return { data: null, error }
-    } finally {
-      if (opts?.showLoading) toggleLoading()
-    }
+    return await $fetch<ResT>(request, {
+      ...opts,
+      onRequest: ({ request }) => {
+        console.debug(request)
+        if (opts?.showLoading) toggleLoading()
+        if (opts?.loadingMessage) showAlert({ 
+            type: "info", 
+            title: opts.loadingMessage
+          })
+      },
+      onRequestError: ({ error }) => {
+        console.error(error)
+        if (opts?.showLoading) toggleLoading()
+        showAlert({ 
+          type: "error", 
+          title: error.message,
+          error: error && errorToString(error)
+        })
+      },
+      onResponse: ({ response }) => {
+        console.debug(response)
+        if (opts?.showLoading) toggleLoading()
+        if (opts?.successMessage) showAlert({ 
+          type: "success", 
+          title: opts.successMessage
+        })
+      },
+      onResponseError: ({ response }) => {
+        console.error(response._data)
+        const error = response._data as CustomError
+        if (opts?.showLoading) toggleLoading()
+        showAlert({
+          type: "error", 
+          title: error.message || t("errors.default"),
+          error: error && errorToString(error)
+        })
+      }
+    })
   }
 }
