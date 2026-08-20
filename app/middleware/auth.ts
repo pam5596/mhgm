dfe4d4d1b4b48320a9f5bc5e47240f7014f85d64
pam5596 <ryosuke3191@gmail.com>
2026-08-21 02:15:44 +0900

    #51 authミドルウェアを適応

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,10 +1,7 @@
 export default defineNuxtRouteMiddleware(() => {
   const { loggedIn } = useUserSession()
-  const { showAlert } = useAlert()
-  const { t } = useI18n()
 
   if (!loggedIn.value) {
-    showAlert({ type: 'error', title: t('errors.required_login') })
     return navigateTo('/')
   }
 })
