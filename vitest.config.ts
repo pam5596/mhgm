25e884d4a1fb287a9859e110bd2715e71ebc5f9a
pam5596 <ryosuke3191@gmail.com>
2026-08-30 07:46:07 +0000

    #80 vitestにauto-import機能を実装

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,41 @@
 import { defineVitestProject } from "@nuxt/test-utils/config";
 import { defineConfig } from "vitest/config";
+import AutoImport from "unplugin-auto-import/vite"
+import { resolve } from 'node:path'
 
 export default defineConfig({
+	plugins: [
+		AutoImport({
+      imports: [
+        'vitest',
+        {
+          'h3': [
+            'defineEventHandler',
+            'getRouterParam',
+            'getQuery',
+            'readBody',
+            'createError',
+            'setResponseStatus',
+            'sendError',
+            // 他に使っているh3関数があれば追加
+          ],
+        },
+      ],
+      dirs: [
+        resolve(__dirname, './shared/models'),
+        resolve(__dirname, './shared/models/interfaces'),
+        resolve(__dirname, './shared/errors'),
+        resolve(__dirname, './shared/enums'),
+        resolve(__dirname, './shared/dtos'),
+        resolve(__dirname, './server/repositories'),
+        resolve(__dirname, './server/clients'),
+        resolve(__dirname, './server/services'),
+        resolve(__dirname, './server/instances'),
+      ],
+      dts: resolve(__dirname, './vitest-auto-imports.d.ts'),
+      vueTemplate: false,
+    }),
+	],
 	resolve: {
 		tsconfigPaths: true,
 	},
@@ -9,20 +43,25 @@ export default defineConfig({
 		globals: true,
 		projects: [
 			{
+				extends: true,
 				test: {
 					name: "unit",
 					include: ["test/unit/**/*.{test,spec}.ts"],
 					environment: "node",
+					globals: true
 				},
 			},
 			{
+				extends: true,
 				test: {
 					name: "integration",
 					include: ["test/integration/**/*.{test,spec}.ts"],
 					environment: "node",
+					globals: true
 				},
 			},
 			{
+				extends: true,
 				test: {
 					name: "e2e",
 					include: ["test/e2e/**/*.{test,spec}.ts"],
