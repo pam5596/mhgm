e631b0f47d1593e29a53d7ca647fe72680acc1ca
pam5596 <ryosuke3191@gmail.com>
2026-09-12 21:08:10 +0900

    #98 integrationテストを分離

--- test/clients/google.test.ts
+++ test/clients/google.test.ts
@@ -1,6 +1,3 @@
-import { describe, expect, it } from "vitest";
-import { GoogleClient } from "../../../server/clients/google";
-
 describe("PrismaORMClient動作確認", async () => {
 	it.skip("環境変数からデータベースURLを取得できる", () => {
 		console.log(process.env.GOOGLE_ACCESS_TOKEN);
