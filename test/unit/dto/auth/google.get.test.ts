0989a467463b60847705a5c776df0e80e508eba0
pam5596 <ryosuke3191@gmail.com>
2026-09-12 20:52:26 +0900

    #98 unitテストを削除

--- test/unit/dto/auth/google.get.test.ts
+++
@@ -1,9 +0,0 @@
-import { zocker } from "zocker"
-
-describe("AuthGoogleGETRequestDTOの単体テスト", () => {
-	it("RequestDTOが作成できる", () => {
-		const mock = zocker(AuthGoogleGETRequestDTO.schema()).generate()
-
-		expect(() => new AuthGoogleGETRequestDTO(mock)).not.toThrow();
-	});
-});
--- test/unit/dto/auth/public/user/settings/event-messages/index.patch.ts
+++
@@ -1,9 +0,0 @@
-import { zocker } from "zocker";
-
-describe("AuthPublicUsersEventMessagesPATCHの単体テスト", () => {
-  it("RequestDTOが作成できる", () => {
-    const mock = zocker(AuthPublicUsersEventMessagesPATCH.schema()).generate();
-
-    expect(() => new AuthPublicUsersEventMessagesPATCH(mock)).not.toThrow();
-  });
-});
